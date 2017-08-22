ffbc6ee4a0b1a63b4d7ffdab53f79f9e7033534b
Alexey Shulga <levsha128@gmail.com>
2017-08-22 15:06:56 +0300

    implement items adding and deleting, add test data

--- app/overview/overview.js
+++ app/overview/overview.js
@@ -2,13 +2,45 @@
 
 angular.module('myApp.overview', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/overview', {
-    templateUrl: 'overview/overview.html',
-    controller: 'OverviewCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/overview', {
+            templateUrl: 'overview/overview.html',
+            controller: 'OverviewCtrl'
+        });
+    }])
 
-.controller('OverviewCtrl', [function() {
-
-}]);
+    .controller('OverviewCtrl', function ($scope) {
+        $scope.newItem = '';
+        console.log($scope);
+        $scope.items = [
+            {
+                title: '1111',
+                comments: [{}, {}]
+            },
+            {
+                title: '222',
+                comments: []
+            }
+        ];
+        $scope.hasItem = function (title) {
+            return $scope.items.some(function (item) {
+                return item.title === title
+            });
+        }
+        $scope.addItem = function () {
+            if ($scope.newItem === '' || $scope.hasItem($scope.newItem)) {
+                return;
+            }
+            $scope.items.push({
+                title: $scope.newItem,
+                comments: []
+            });
+            $scope.resetInput();
+        };
+        $scope.resetInput = function () {
+            $scope.newItem = '';
+        };
+        $scope.deleteItem = function (index) {
+            console.log(index);
+        }
+    });
