3373b5bbe32c52914e9cac8d5b5433bd02bc7cf5
Alexey Shulga <levsha128@gmail.com>
2017-08-22 16:44:20 +0300

    implement comments display and adding display test user

--- app/overview/overview.js
+++ app/overview/overview.js
@@ -15,11 +15,29 @@ angular.module('myApp.overview', ['ngRoute'])
         $scope.items = [
             {
                 title: '1111',
-                comments: [{}, {}]
+                comments: [{
+                    author: {
+                        name: 'User 1',
+                        avatar: 'user1.jpg'
+                    },
+                    text: 'test comment'
+                }, {
+                    author: {
+                        name: 'User 2',
+                        avatar: 'user2.jpg'
+                    },
+                    text: 'test comment2'
+                }]
             },
             {
                 title: '222',
-                comments: []
+                comments: [{
+                    author: {
+                        name: 'User 1',
+                        avatar: 'user1.jpg'
+                    },
+                    text: 'test comment3'
+                }]
             }
         ];
         $scope.hasItem = function (title) {
@@ -40,7 +58,11 @@ angular.module('myApp.overview', ['ngRoute'])
             $scope.selectItem(item);
         };
         $scope.selectItem = function (item) {
+            if ($scope.selectedItem === item) {
+                item = null;
+            }
             $scope.selectedItem = item;
+            $scope.comments = $scope.selectedItem ? $scope.selectedItem.comments : [];
         };
         $scope.resetInput = function () {
             $scope.newItem = '';
@@ -53,10 +75,32 @@ angular.module('myApp.overview', ['ngRoute'])
         };
         $scope.resetSelection = function () {
             $scope.selectedItem = null;
-        }
+            $scope.comments = $scope.selectedItem ? $scope.selectedItem.comments : [];
+        };
         $scope.keypressHandler = function (event) {
             if (event.which === 13) { // on enter
                 $scope.addItem();
             }
+        };
+
+        $scope.comments = [];
+        $scope.user = {
+            name: 'User 1',
+            avatar: 'user1.jpg'
+        };
+        $scope.newComment = '';
+        $scope.getSelectedItemIndex = function () {
+            return $scope.items.indexOf($scope.selectedItem);
+        };
+        $scope.commentKeypressHandler = function (event) {
+            if (event.ctrlKey && (event.which === 13 || event.which === 10)) { // on ctrl+enter //todo check on mobile devices
+                $scope.addComment($scope.comments, $scope.user, $scope.newComment);
+            }
+        };
+        $scope.addComment = function (comments, user, text) {
+            comments.push({
+                author: user,
+                text: text
+            });
         }
     });
